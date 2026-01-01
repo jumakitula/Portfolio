985c4aba706b2dba8b92575ef811f012dc3deff0
Juma Kitula <jumakitula0@gmail.com>
2026-01-01 16:53:06 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -52,7 +52,7 @@ export default function Hero() {
             <div className="w-48 h-48 rounded-full overflow-hidden border-4 border-cyan-400 float bg-slate-800 flex items-center justify-center">
               {/* Fallback image path used to prevent build failure */}
               <img
-                src={profilePic}
+                src="./profile_picture.jpg";
                 alt="Juma Bakari"
                 className="w-full h-full object-cover"
                 onError={(e) => {
